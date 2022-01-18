b96e019b025c3680635517f0b21d9530546715d6
Oskar Abrahamsson <oskarabrahamsson@users.noreply.github.com>
2022-01-18 18:58:43 +0100

    Make the pretty-printer output app_lists

--- pretty.ml
+++ pretty.ml
@@ -9,44 +9,40 @@ let rec breakdist after = function
   | String s :: es -> String.length s + breakdist after es
   | Break _ :: _ -> 0
 
-let print out_chan margin tok =
+let print margin tok =
   let space = ref margin in
   let blanks n =
     begin
-      output_string out_chan (String.concat "" (List.init 10 (fun _ -> " ")));
-      space := !space - n
+      space := !space - n;
+      App_list.list (List.init n (fun _ -> " "))
     end in
   let newline () =
     begin
-      output_string out_chan "\n";
-      space := margin
+      space := margin;
+      App_list.list ["\n"]
     end in
   let rec printing blockspace after toks =
     match toks with
     | Block (bes, indent, len) :: es ->
-        begin
-          printing indent len bes;
-          printing blockspace after es
-        end
+        let out1 = printing indent len bes in
+        let out2 = printing blockspace after es in
+          App_list.append out1 out2
     | String s :: es ->
-        begin
-          output_string out_chan s;
-          space := !space - String.length s;
-          printing blockspace after es
-        end
+        let _ = space := !space - String.length s in
+        let out1 = App_list.list [s] in
+        let out2 = printing blockspace after es in
+          App_list.append out1 out2
     | Break len :: es ->
         if len + breakdist after es <= !space then
-          begin
-            blanks len;
-            printing blockspace after es
-          end
+          let out1 = blanks len in
+          let out2 = printing blockspace after es in
+            App_list.append out1 out2
         else
-          begin
-            newline ();
-            blanks (margin - blockspace);
-            printing blockspace after es
-          end
-    | [] -> () in
+          let out1 = newline () in
+          let out2 = blanks (margin - blockspace) in
+          let out3 = printing blockspace after es in
+            App_list.append out1 (App_list.append out2 out3)
+    | [] -> App_list.empty in
   printing margin 0 [tok]
 
 let string s = String s
--- pretty.mli
+++ pretty.mli
@@ -3,5 +3,5 @@ type token
 val string : string -> token
 val break : int -> token
 val block : int -> token list -> token
-val print : out_channel -> int -> token -> unit
+val print : int -> token -> string App_list.app_list
 
